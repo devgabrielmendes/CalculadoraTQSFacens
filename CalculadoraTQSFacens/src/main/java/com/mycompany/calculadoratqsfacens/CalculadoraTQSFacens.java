57a5ac11cc6b3869143bf55f50028c09269343d8
Gabriel Mendes <105362932+devgabrielmendes@users.noreply.github.com>
2023-11-06 16:54:20 -0300

    Update CalculadoraTQSFacens.java

--- CalculadoraTQSFacens/src/main/java/com/mycompany/calculadoratqsfacens/CalculadoraTQSFacens.java
+++ CalculadoraTQSFacens/src/main/java/com/mycompany/calculadoratqsfacens/CalculadoraTQSFacens.java
@@ -3,6 +3,12 @@ package com.mycompany.calculadoratqsfacens;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ *@author Gabriel Mendes da Silva
+ *data 06/11/2023
+ *@version 1.2
+ */
+
 public class CalculadoraTQSFacens {
 
     public static void main(String[] args) {
